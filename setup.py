e21710420591f6674dca83cb4add71fe60eda132
Kirti Anand <kirtianand028@gmail.com>
2024-10-16 23:22:43 +0200

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MedicalChatbotLLM',
+    version = '0.0.0',
+    author = 'Kirti Anand',
+    author_email = 'kirtijayganeshanand@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
